b5c34b6c7acd84eb051ebb6a694d16ad52e6650e
mercersFIRE <imranurrahman16@gmail.com>
2024-09-23 10:48:21 +0600

    23-9-24

---
+++ 1st.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+#define pb push_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+using namespace std;
+typedef long long ll;
+
+int solve()
+{
+    ll n, x,sum=0;
+    cin>>n;
+    
+    return 0;
+}
+
+int main()
+{
+    fast;
+    ll tc = 1;cin>>tc;
+    while (tc--)
+    {
+        if (solve()){
+            //cout << "Yes\n";
+        }
+        else{
+            //cout << "No\n";
+        }
+    }
+    return 0;
+}
