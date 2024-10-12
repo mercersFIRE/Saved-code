fe94c50c3bda68f76a66d8522eb94d4eac38bf61
mercersFIRE <imranurrahman16@gmail.com>
2024-10-12 22:38:18 +0600

    1203C - Common Divisors

--- 1st.cpp
+++ 1st.cpp
@@ -11,16 +11,34 @@ typedef long long ll;
 
 int solve()
 {
-    ll n, x,sum=0;
+    ll n, x,sum=0,ans=1;
     cin>>n;
-    
+    std::vector<ll> v(n);
+    for (int i = 0; i < n; ++i)
+    {
+    	cin>>v[i];
+    	x=(i?__gcd(v[i],x):v[i]);
+    }
+    //cout<<x<<endl;
+    n=x;
+    for (int i = 2; i <=n; ++i)
+    {
+    	sum=0;
+    	while(n%i==0)
+    	{
+    		sum++;
+    		n/=i;
+    	}
+    	ans*=(sum+1);
+    }
+    cout<<ans<<endl;
     return 0;
 }
 
 int main()
 {
     fast;
-    ll tc = 1;cin>>tc;
+    ll tc = 1;//cin>>tc;
     while (tc--)
     {
         if (solve()){
