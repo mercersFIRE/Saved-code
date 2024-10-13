4ca7c1852655f552daeeb118e7b5d9be9643bf8a
mercersFIRE <imranurrahman16@gmail.com>
2024-10-13 18:37:19 +0600

    solved

---
+++ Gym - 310911H.cpp
@@ -0,0 +1,42 @@
+#include "bits/stdc++.h"
+#define pb push_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+using namespace std;
+typedef long long ll;
+
+int solve()
+{
+    ll n, x,sum=0,m,l,r;
+    cin>>n>>m>>l>>r;
+    x=(n*m)/__gcd(n,m);
+    l=max(l,x);
+    if(l%x!=0)
+    {
+    	l+=(x-(l%x));
+    }
+    if (l>r)
+        return 0;
+    cout<<((r-l)/x)+1<<endl;
+    return 1;
+}
+
+int main()
+{
+    fast;
+    ll tc = 1;//cin>>tc;
+    while (tc--)
+    {
+        if (solve()){
+            //cout << "Yes\n";
+        }
+        else{
+            cout << "0\n";
+        }
+    }
+    return 0;
+}
