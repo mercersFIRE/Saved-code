d1e495d318267beb126ba2404e166db111d61415
mercersFIRE <imranurrahman16@gmail.com>
2024-10-13 15:01:47 +0600

    number theory

--- Gym - 310911E.cpp
+++ Gym - 310911E.cpp
@@ -11,15 +11,9 @@ typedef long long ll;
 
 int solve()
 {
-    ll n, x,sum=0,ans=1;
+    ll n, x,sum=0;
     cin>>n;
     std::vector<ll> v(n),prime(1000006),p;
-    for (int i = 0; i < n; ++i)
-    {
-    	cin>>v[i];
-    	x=(i?__gcd(v[i],x):v[i]);
-    }
-    n=x;
     for (ll i = 2; i<= 1e6; ++i)
     {
     	if(prime[i]==0)
@@ -34,18 +28,23 @@ int solve()
     {
     	if(!prime[i])p.pb(i);
     }
-    for (int i = 0; i<p.size() and p[i] <=n; ++i)
+    for (int i = 0; i < n; ++i)
     {
-    	sum=0;
-    	while(n%p[i]==0)
-    	{
-    		sum++;
-    		n/=p[i];
-    	}
-    	ans*=(sum+1);
+    	cin>>v[i];
+        std::map<ll, ll> m;
+        for (int j = 0; j < p.size()&&p[j]<=x; ++j)
+        {
+            x=v[i];
+            while(x%p[j]==0)
+            {
+                cout<<" found "<<p[j]<<endl;
+                m[p[j]]++;x/=p[j];
+            }
+        }
+        for(auto y:m)
+            cout<<y.ff<<" "<<y.ss<<endl;
     }
-    if(n>1)ans*=2;
-    cout<<ans<<endl;
+    
     return 0;
 }
 
