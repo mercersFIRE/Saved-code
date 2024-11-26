9015a76b040bb0616a5b9353944941f442abf88b
mercersFIRE <imranurrahman16@gmail.com>
2024-11-26 17:18:53 +0600

    solved old one

---
+++ CodeForces - 1285C.cpp
@@ -0,0 +1,50 @@
+#include "bits/stdc++.h"
+#define pb push_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+using namespace std;
+typedef long long ll;
+
+const ll N=1e6+7;
+vector<ll>prime,spf(N);
+void seive()
+{
+    for (ll i = 1; i < N; ++i)spf[i]=i;
+    for (ll i = 2; i*i<N; ++i)
+    {
+        if(spf[i]==i)
+            for (ll j = i*i; j < N; j+=i) 
+                spf[j]=min(i,spf[j]);
+    }
+    for (int i = 2; i < N; ++i)if(spf[i]==i)prime.pb(i);
+}
+
+int solve()
+{
+    ll n, x,sum=0;
+    cin>>n;
+    x=sqrtl(n);
+    while(!(n%x==0&&(__gcd(x,n/x))==1))x--;
+    cout<<n/x<<" "<<x<<endl;
+    return 0;
+}
+
+int main()
+{
+    fast;
+    ll tc = 1;//cin>>tc;
+    while (tc--)
+    {
+        if (solve()){
+            //cout << "Yes\n";
+        }
+        else{
+            //cout << "No\n";
+        }
+    }
+    return 0;
+}
